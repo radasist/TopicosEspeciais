d47875eb61f0f7f4c3354958dbbba33690b6d2a1
radasist <radasist@gmail.com>
2015-05-02 23:54:05 -0300

    Atualizado dump do banco

--- topicos_especiais.sql
+++ topicos_especiais.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 01-Maio-2015 às 16:56
+-- Generation Time: 02-Maio-2015 às 19:48
 -- Versão do servidor: 5.5.42
 -- PHP Version: 5.4.40
 
@@ -34,14 +34,17 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `senha` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `permissao` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `data_nasc`, `sexo`, `usuario`, `senha`, `permissao`) VALUES
-(1, 'Rafael Streit', '1989-12-31', 'masc', 'rafael', 'rafael', 'a');
+(1, 'Rafael Streit', '1989-12-31', 'masc', 'rafael', 'rafael', 'a'),
+(2, 'Ariana', '1990-01-01', 'fem', 'ariana', 'ariana', 'a'),
+(3, 'Ariel', '1990-01-01', 'masc', 'ariel', 'ariel', 'a'),
+(4, 'Fabio', '1990-01-01', 'masc', 'fabio', 'fabio', 'a');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +64,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
