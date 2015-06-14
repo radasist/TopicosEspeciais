a939a551955372bb40cd39393d497c1bf66b1278
radasist <radasist@gmail.com>
2015-06-14 00:19:02 -0300

    Criada validação pra não permitir que dois emails iguas sejam cadastrados no sistema.

--- Novo/cadastroCliente.php
+++ Novo/cadastroCliente.php
@@ -10,7 +10,14 @@ if ($_POST["id"]) {
 
 if ($_POST["enviado"]) {
     if ($_POST["id"] == "0") {
-        $result = $sqlConn->query ("INSERT INTO usuarios (nome, data_nasc, sexo, email, senha, permissao) VALUES ('".$_POST["nome"]."', '".$_POST["datanasc"]."', '".$_POST["sexo"]."', '".$_POST["email"]."', '".md5($_POST["senha"])."', '".$_POST["permissao"]."')");
+        $emailCadastrado = $sqlConn->query ("SELECT 1 FROM usuarios WHERE email = '".$_POST["email"]."'");
+        $resultEmailCadastrado = $emailCadastrado->fetch_all(MYSQLI_ASSOC);
+
+        if (count($resultEmailCadastrado) == 0) {
+            $result = $sqlConn->query ("INSERT INTO usuarios (nome, data_nasc, sexo, email, senha, permissao) VALUES ('".$_POST["nome"]."', '".$_POST["datanasc"]."', '".$_POST["sexo"]."', '".$_POST["email"]."', '".md5($_POST["senha"])."', '".$_POST["permissao"]."')");
+        } else {
+            $result = 2;
+        }
     } else {
         if ($_POST["senha"] != "") {
             $result = $sqlConn->query ("UPDATE usuarios SET nome = '".$_POST["nome"]."', data_nasc = '".$_POST["datanasc"]."', sexo = '".$_POST["sexo"]."', email = '".$_POST["email"]."', senha = '".md5($_POST["senha"])."', permissao = '".$_POST["permissao"]."' WHERE id = '$id'");
@@ -22,6 +29,10 @@ if ($_POST["enviado"]) {
     $result = -1;
 }
 
+$labelTitle = "Cadastro";
+$labelSubmit = "Cadastrar";
+$passwordRequired = "required";
+
 if ($_POST["id"]) {
     $data = $sqlConn->query("SELECT * FROM usuarios WHERE id = '$id'");
     $resultArray = $data->fetch_all(MYSQLI_ASSOC);
@@ -31,11 +42,21 @@ if ($_POST["id"]) {
     $sexo = $resultArray[0]["sexo"];
     $email = $resultArray[0]["email"];
     $senha = "";
+    $senha2 = "";
     $permissao = $resultArray[0]["permissao"];
 
     $labelTitle = "Alteração";
     $labelSubmit = "Salvar";
     $passwordRequired = "";
+} else if ($result == 2) {
+    $id = "0";
+    $nome = $_POST["nome"];
+    $datanasc = $_POST["datanasc"];
+    $sexo = $_POST["sexo"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $senha2 = $_POST["senha2"];
+    $permissao = $_POST["permissao"];
 } else {
     $id = "0";
     $nome = "";
@@ -43,10 +64,8 @@ if ($_POST["id"]) {
     $sexo = "";
     $email = "";
     $senha = "";
-
-    $labelTitle = "Cadastro";
-    $labelSubmit = "Cadastrar";
-    $passwordRequired = "required";
+    $senha2 = "";
+    $permissao = "";
 }
 
 $sexomasc == "";
@@ -86,7 +105,7 @@ if ($permissao == "a") {
     <label for="senha">Senha</label>
     <input type="password" id="senha" name="senha" placeholder="Senha" value="<?=$senha?>" <?=$passwordRequired?>>
     <label for="senha2">Repita a senha</label>
-    <input type="password" id="senha2" name="senha2" placeholder="Repita a senha" value="<?=$senha?>" <?=$passwordRequired?>>
+    <input type="password" id="senha2" name="senha2" placeholder="Repita a senha" value="<?=$senha2?>" <?=$passwordRequired?>>
     <label for="permissao">Permissão</label>
     <select id="permissao" name="permissao" required>
         <option value="">Selecione...</option>
@@ -117,5 +136,7 @@ if ($permissao == "a") {
         } else {
             showMessage("error", "Ocorreu um erro e não foi possível salvar a alteração!");
         }
+    } else if (<?=(string)$result?> == 2) {
+        showMessage("error", "Email já cadastrado no sistema!");
     }
 </script>
