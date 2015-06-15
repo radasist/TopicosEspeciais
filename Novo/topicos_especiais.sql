118679996fb812261a03045fa93cd45f5c522469
radasist <radasist@gmail.com>
2015-06-15 00:20:13 -0300

    Feito ajueste no css da página principal, correção na página de cadastro de produtos, correção em relação a permissão de página e criada...
    ...trigger que dá o bônus pros que indicaram e de forma recorrente.

--- Novo/topicos_especiais.sql
+++ Novo/topicos_especiais.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 13-Jun-2015 às 19:58
+-- Generation Time: 14-Jun-2015 às 20:16
 -- Versão do servidor: 5.5.42
 -- PHP Version: 5.4.40
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `configuracoes` (
 --
 
 INSERT INTO `configuracoes` (`nivelbonus`, `porcentagembonus`) VALUES
-(3, '10.00');
+(2, '15.00');
 
 -- --------------------------------------------------------
 
@@ -67,7 +67,7 @@ INSERT INTO `estabelecimentos` (`id`, `nome`, `razaosocial`, `grupo`) VALUES
 CREATE TABLE IF NOT EXISTS `grupoestabelecimentos` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `grupoestabelecimentos`
@@ -88,15 +88,18 @@ CREATE TABLE IF NOT EXISTS `indicacao` (
   `indica` int(11) NOT NULL,
   `indicado` int(11) NOT NULL DEFAULT '0',
   `emailindicado` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `indicacao`
 --
 
 INSERT INTO `indicacao` (`id`, `indica`, `indicado`, `emailindicado`) VALUES
-(2, 1, 0, 'feliciamegue@gmail.com'),
-(3, 1, 37, 'barbarastreit@gmail.com');
+(5, 1, 41, 'barbarastreit@gmail.com'),
+(6, 41, 42, 'feliciamegue@gmail.com'),
+(7, 42, 43, 'gersonstreit@gmail.com'),
+(8, 43, 44, 'megue@gmail.com'),
+(9, 44, 45, 'isis@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -110,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `movimentos` (
   `produto` int(11) NOT NULL,
   `quantidade` decimal(10,3) NOT NULL,
   `total` decimal(10,2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `movimentos`
@@ -119,7 +122,71 @@ CREATE TABLE IF NOT EXISTS `movimentos` (
 INSERT INTO `movimentos` (`id`, `cliente`, `produto`, `quantidade`, `total`) VALUES
 (1, 1, 1, '1.750', '7.00'),
 (2, 1, 2, '0.001', '20.00'),
-(3, 3, 1, '5.509', '22.04');
+(3, 3, 1, '5.509', '22.04'),
+(4, 1, 1, '1.000', '10.00'),
+(5, 45, 1, '1.000', '10.00'),
+(6, 45, 1, '1.000', '10.00'),
+(7, 45, 2, '1.000', '20000.00'),
+(8, 45, 1, '1.000', '10.00'),
+(9, 45, 1, '1.000', '10.00'),
+(10, 45, 1, '1.000', '10.00'),
+(11, 45, 1, '2.000', '20.00'),
+(12, 45, 1, '1.000', '10.00'),
+(13, 45, 1, '1.000', '10.00'),
+(14, 45, 1, '1.000', '10.00'),
+(15, 45, 1, '1.000', '10.00'),
+(16, 45, 1, '1.000', '10.00'),
+(17, 45, 1, '1.000', '10.00'),
+(18, 45, 1, '1.000', '10.00'),
+(19, 45, 1, '1.000', '10.00'),
+(20, 45, 1, '1.000', '10.00'),
+(21, 45, 1, '1.000', '10.00'),
+(22, 45, 1, '1.000', '10.00'),
+(23, 45, 1, '1.000', '10.00'),
+(24, 45, 1, '1.000', '10.00'),
+(25, 45, 1, '1.000', '10.00'),
+(26, 45, 1, '1.000', '10.00'),
+(27, 45, 1, '1.000', '10.00'),
+(28, 45, 1, '1.000', '10.00'),
+(29, 45, 1, '1.000', '10.00'),
+(30, 45, 1, '1.000', '10.00'),
+(31, 45, 1, '1.000', '10.00'),
+(32, 45, 1, '1.000', '10.00');
+
+--
+-- Acionadores `movimentos`
+--
+DELIMITER $$
+CREATE TRIGGER `setBonus` AFTER INSERT ON `movimentos`
+ FOR EACH ROW BEGIN
+
+	DECLARE recorrencia_bonus INT default 0;
+	DECLARE porcentagem_bonus DECIMAL(10,2) default 0;
+	DECLARE loop_i INT default 0;
+	DECLARE saldo_bonus DECIMAL(10,2) default 0;
+	DECLARE bonus_acumulado DECIMAL(10,2) default 0;
+	DECLARE recebe_bonus INT default 0;
+
+	SET @loop_i := 0;
+	SET @recorrencia_bonus := (SELECT nivelbonus FROM configuracoes LIMIT 1);
+	SET @porcentagem_bonus := (SELECT porcentagembonus FROM configuracoes LIMIT 1);
+	SET @recebe_bonus := (SELECT indica FROM indicacao WHERE indicado = NEW.cliente LIMIT 1);
+
+	WHILE @loop_i < @recorrencia_bonus DO
+
+		SET @saldo_bonus := (SELECT bonus FROM usuarios WHERE id = @recebe_bonus LIMIT 1);
+		SET @bonus_acumulado := @saldo_bonus + (NEW.total * (@porcentagem_bonus / 100));
+
+	   	UPDATE usuarios SET bonus = @bonus_acumulado WHERE id = @recebe_bonus;
+
+	   	SET @recebe_bonus := (SELECT indica FROM indicacao WHERE indicado = @recebe_bonus);
+        SET @loop_i := @loop_i + 1;
+
+	END WHILE;
+
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -139,7 +206,7 @@ CREATE TABLE IF NOT EXISTS `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `valor`, `estabelecimento`) VALUES
-(1, 'Gasolina', '4.00', 8),
+(1, 'Gasolina', '10.00', 8),
 (2, 'Alcool', '20000.00', 8);
 
 -- --------------------------------------------------------
@@ -157,14 +224,14 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `senha` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
   `bonus` decimal(10,2) NOT NULL DEFAULT '0.00',
   `permissao` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `data_nasc`, `sexo`, `email`, `senha`, `bonus`, `permissao`) VALUES
-(1, 'Rafael Streit', '1989-12-31', 'masc', 'radasist@gmail.com', '9135d8523ad3da99d8a4eb83afac13d1', '0.00', 'a'),
+(1, 'Rafael Streit', '1989-12-31', 'masc', 'radasist@gmail.com', '9135d8523ad3da99d8a4eb83afac13d1', '3.00', 'a'),
 (2, 'Ariana', '1990-01-01', 'fem', 'ariana', 'ariana', '0.00', 'a'),
 (3, 'Ariel', '1990-01-01', 'masc', 'arielfeiber@faccat.br', 'ariel', '0.00', 'a'),
 (4, 'Fabio', '1990-01-01', 'masc', 'fabio', 'fabio', '0.00', 'a'),
@@ -191,7 +258,12 @@ INSERT INTO `usuarios` (`id`, `nome`, `data_nasc`, `sexo`, `email`, `senha`, `bo
 (34, 'Planta do vaso de Planta', '2015-06-04', 'fem', 'planta@planta.com', '123', '0.00', 'a'),
 (35, 'TS', '0001-01-01', 'fem', 't@s.com', 'caf1a3dfb505ffed0d024130f58c5cfa', '0.00', 'f'),
 (36, 'teste', '0011-11-11', 'masc', '11@11.11', '6512bd43d9caa6e02c990b0a82652dca', '0.00', 'f'),
-(37, 'Barbara', '1986-06-25', 'fem', 'barbarastreit@gmail.com', '4d6c4d6b5b6c7fd2c43727ce32a56f4e', '0.00', 'c');
+(39, 'Barba', '0002-02-02', 'fem', 'barbarasqtreit@gmail.com', 'c4ca4238a0b923820dcc509a6f75849b', '0.00', 'a'),
+(41, 'Barbara', '1986-06-25', 'fem', 'barbarastreit@gmail.com', '4d6c4d6b5b6c7fd2c43727ce32a56f4e', '4.50', 'a'),
+(42, 'Terezinha', '1965-01-01', 'fem', 'feliciamegue@gmail.com', 'eb4a4a36e4d53916f9979759c3d3b822', '6.00', 'a'),
+(43, 'Gerson', '1955-06-22', 'masc', 'gersonstreit@gmail.com', 'd828a5b9b09b334ce76bf241ca16c4eb', '7.50', 'a'),
+(44, 'Megue', '2002-04-22', 'fem', 'megue@gmail.com', '548c0e2986f152f5ad7203aae24be45d', '7.50', 'a'),
+(45, 'Isis', '2010-01-01', 'fem', 'isis@gmail.com', '529419940a585fb2a83765b2ca5cc091', '0.00', 'a');
 
 --
 -- Acionadores `usuarios`
@@ -257,17 +329,17 @@ ALTER TABLE `estabelecimentos`
 -- AUTO_INCREMENT for table `grupoestabelecimentos`
 --
 ALTER TABLE `grupoestabelecimentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `indicacao`
 --
 ALTER TABLE `indicacao`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `movimentos`
 --
 ALTER TABLE `movimentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `produtos`
 --
@@ -277,7 +349,7 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
 --
 -- Constraints for dumped tables
 --
@@ -292,8 +364,8 @@ ADD CONSTRAINT `fk_grupoestabelecimento` FOREIGN KEY (`grupo`) REFERENCES `grupo
 -- Limitadores para a tabela `movimentos`
 --
 ALTER TABLE `movimentos`
-ADD CONSTRAINT `fk_mv_produto` FOREIGN KEY (`produto`) REFERENCES `produtos` (`id`),
-ADD CONSTRAINT `fk_mv_cliente` FOREIGN KEY (`cliente`) REFERENCES `usuarios` (`id`);
+ADD CONSTRAINT `fk_mv_cliente` FOREIGN KEY (`cliente`) REFERENCES `usuarios` (`id`),
+ADD CONSTRAINT `fk_mv_produto` FOREIGN KEY (`produto`) REFERENCES `produtos` (`id`);
 
 --
 -- Limitadores para a tabela `produtos`
